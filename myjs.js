42f6495e613edac408ac1352fa6db37c4907634d
iochel <126267673+iochel@users.noreply.github.com>
2023-05-16 09:16:15 +0800

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -25,4 +25,27 @@ function agecompare(ageverify) {
 }
     Age = getAge();
     console.log(agecompare(Age));
-    
+
+
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = "";
+    document.getElementById('result').innerHTML = "";
+}
+
+function performCalculation(event) {
+    if (event && event.keyCode !== 13) {
+        return;
+    }
+    var input = document.getElementById('display').value;
+    if (input) {
+        var result = eval(input);
+        document.getElementById('display').value = result;
+        document.getElementById('result').innerHTML = "Result: " + result;
+    } else {
+        document.getElementById('result').innerHTML = "";
+    }
+}
